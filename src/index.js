ad99fc60f046f28888cb65a87e4cf075fda54351
poamen <poamen@appzonegroup.com>
2023-08-04 18:10:08 +0100

    basic sdk test

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MeProtocolProvider } from 'me-protocol';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<MeProtocolProvider
+                  email={"mavapas530@inkiny.com"}
+                  meApiKey={"b5hnok5zcoongvz406l8"}
+                >
+  <App />
+</MeProtocolProvider>
   </React.StrictMode>
 );
 
